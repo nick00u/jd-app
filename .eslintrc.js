b01ccd095c109a7297202b1d42b437dff89b24db
nick00u <nsnicksn@gmail.com>
2025-01-10 22:39:39 +0800

    jd-app

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,13 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  },
+
+  parserOpts: {
+    plugins: ["jsx"]
+  },
+  "parser": '@babel/eslint-parser',
+  "parserOptions": {
+    "requireConfigFile": false,
   }
 }
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  // 在vue.config.js中添加，表示不进行eslint验证即可
+  lintOnSave: false
 })
